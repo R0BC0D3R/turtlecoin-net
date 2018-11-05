e950857b04612701a01c6c528259610c1e1fbed6
R0BC0D3R <R0BC0D3R@gmail.com>
2018-11-05 09:43:21 -0500

    Add back files removed after removing duplicate directory (different caps)

---
+++ src/ZedWallet/CommandImplementations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Copyright (c) 2018, The TurtleCoin Developers
+// 
+// Please see the included LICENSE file for more information.
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+
+
+#if ! MSVC
+#endif
---
+++ src/ZedWallet/Menu.h.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) 2018, The TurtleCoin Developers
+// 
+// Please see the included LICENSE file for more information.
+
+
+
+//C++ TO C# CONVERTER TODO TASK: The original C++ template specifier was replaced with a C# generic specifier, which may not produce the same behavior:
+//ORIGINAL LINE: template<typename T>
